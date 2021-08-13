37795cb1739ef6055f048dd22472d2968d91d549
Jorge <jlimajorge@gmail.com>
2021-08-13 18:25:39 -0300

    feat: layout login

--- frontend/src/presentation/pages/signin/signin.tsx
+++ frontend/src/presentation/pages/signin/signin.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Card, Row, Col, Button} from 'react-bootstrap'
 import Breadcrumb from '../../layouts/AdminLayout/Breadcrumb'
 import logoPdb from '../../assets/images/logo-pdb.png'
+import {Authentication} from '../../../domain/usecases/auth/authentication'
+import useScriptRef from '../../hooks/useScriptRef'
 
-const SignIn: React.FC = () => {
+import * as Yup from 'yup'
+import {useFormik} from 'formik'
+
+type Props = {
+  authentication: Authentication
+}
+
+const SignIn: React.FC<Props> = ({authentication}: Props) => {
+  const scriptedRef = useScriptRef()
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      submit: null,
+      isLoading: false,
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().max(255).required('Matricula é obrigatória'),
+      password: Yup.string().max(255).required('Senha é obrigatóra'),
+    }),
+    onSubmit: async (values, {setErrors, setStatus, setSubmitting}) => {
+      try {
+        const response = await authentication.auth({
+          username: values.username,
+          password: values.password,
+        })
+        if (scriptedRef.current) {
+          setStatus({success: true})
+          setSubmitting(false)
+        }
+        // await saveAccount(response.auth_token)
+      } catch (error: any) {
+        console.log(error)
+        setStatus({success: false})
+        setErrors({submit: error})
+        setSubmitting(false)
+      }
+    },
+  })
+
   return (
     <>
       <Breadcrumb />
@@ -14,14 +56,22 @@ const SignIn: React.FC = () => {
               <div className="mb-4">
                 <img src={logoPdb} style={{height: 'auto', width: '60%'}} />
               </div>
-              <form>
+              <form noValidate onSubmit={formik.handleSubmit}>
                 <div className="form-group mb-3">
                   <input
                     className="form-control"
                     placeholder="Matrícula"
-                    name="email"
-                    type="email"
+                    name="username"
+                    type="text"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
                   />
+                  {formik.touched.username && formik.errors.username && (
+                    <small className="text-danger form-text">
+                      {formik.errors.username}
+                    </small>
+                  )}
                 </div>
                 <div className="form-group mb-4">
                   <input
@@ -29,17 +79,27 @@ const SignIn: React.FC = () => {
                     placeholder="Senha"
                     name="password"
                     type="password"
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
                   />
+                  {formik.touched.password && formik.errors.password && (
+                    <small className="text-danger form-text">
+                      {formik.errors.password}
+                    </small>
+                  )}
                 </div>
                 <Row>
                   <Col>
                     <Button
                       className="btn-block"
                       color="primary"
+                      disabled={formik.isSubmitting}
                       size="lg"
                       type="submit"
                       variant="primary">
                       Entrar
+                      <i className="feather icon-log-in" />
                     </Button>
                   </Col>
                 </Row>
