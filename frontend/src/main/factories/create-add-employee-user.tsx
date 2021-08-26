57acda1468ed93e42cef74282c482aae6044df78
Jorge <jlimajorge@gmail.com>
2021-08-26 10:04:29 -0300

    feat: criando pagina para criação de usuario a partir de funcionário

---
+++ frontend/src/main/factories/create-add-employee-user.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {RemoteUserCreateEmployeeUser} from '../../data/usecases'
+import {basUrl} from '../../services'
+import {AuthorizeHttpClientDecorator} from '../decorators'
+import {AxiosHttpClient} from '../../infra/http'
+import {LocalStorageAdapter} from '../../infra/cache'
+import {CreateEmployeeUserPage} from '../../presentation/pages'
+
+export const remoteUserCreateEmployeeUser = (
+  id: string,
+): RemoteUserCreateEmployeeUser => {
+  const authHttpClient = new AuthorizeHttpClientDecorator(
+    new LocalStorageAdapter(),
+    new AxiosHttpClient(),
+  )
+
+  return new RemoteUserCreateEmployeeUser(
+    `${basUrl}/manager/users/${id}/reset_password`,
+    authHttpClient,
+  )
+}
+
+type Props = {
+  employeeId: string
+}
+
+export const CreateAddEmployeeUser: React.FC<Props> = ({employeeId}: Props) => (
+  <CreateEmployeeUserPage
+    createEmployeeUser={remoteUserCreateEmployeeUser(employeeId)}
+  />
+)
--- frontend/src/presentation/pages/resource/employees/index/employee-dt.tsx
+++ frontend/src/presentation/pages/resource/employees/index/employee-dt.tsx
@@ -7,6 +7,7 @@ import {
   CreateEmployeeForm,
   CreateEmployeeEdit,
   CreateUserResetPassword,
+  CreateAddEmployeeUser,
 } from '../../../../../main/factories'
 
 type Props = {
