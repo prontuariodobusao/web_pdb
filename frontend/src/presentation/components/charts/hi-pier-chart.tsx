9829c1eb000bf0cdd98d23198b207cbffb33c992
Jorge <jlimajorge@gmail.com>
2021-08-31 09:13:07 -0300

    feat: configurando opções highcharts

--- frontend/src/presentation/components/charts/hi-pier-chart.tsx
+++ frontend/src/presentation/components/charts/hi-pier-chart.tsx
@@ -1,24 +1,74 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react'
 import * as Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import {ChartModel} from '../../../domain/models/charts-model'
 
+// require('highcharts/modules/exporting')(Highcharts)
+// require('highcharts/modules/export-data')(Highcharts)
+
+// Highcharts.setOptions({
+//   lang: {
+//     months: [
+//       'Janeiro',
+//       'Fevereiro',
+//       'Março',
+//       'Abril',
+//       'Maio',
+//       'Junho',
+//       'Julho',
+//       'Agosto',
+//       'Setembro',
+//       'Outubro',
+//       'Novembro',
+//       'Dezembro',
+//     ],
+//     shortMonths: [
+//       'Jan',
+//       'Fev',
+//       'Mar',
+//       'Abr',
+//       'Mai',
+//       'Jun',
+//       'Jul',
+//       'Ago',
+//       'Set',
+//       'Out',
+//       'Nov',
+//       'Dez',
+//     ],
+//     weekdays: [
+//       'Domingo',
+//       'Segunda',
+//       'Terça',
+//       'Quarta',
+//       'Quinta',
+//       'Sexta',
+//       'Sábado',
+//     ],
+//     downloadCSV: 'Exportar CSV',
+//     downloadXLS: 'Exportar XLS',
+//     viewFullscreen: 'Visualizar em tela cheia',
+//     printChart: 'Imprimir gráfico',
+//   },
+// })
+
 // The wrapper exports only a default component class that at the same time is a
 // namespace for the related Props interface (HighchartsReact.Props). All other
 // interfaces like Options come from the Highcharts module itself.
 
-const pieColors = (base: string) => {
-  const colors = []
-  let i
-  for (i = 0; i < 10; i += 1) {
-    colors.push(
-      Highcharts.color(base)
-        .brighten((i - 3) / 7)
-        .get(),
-    )
-  }
-  return colors
-}
+// const pieColors = (base: string) => {
+//   const colors = []
+//   let i
+//   for (i = 0; i < 10; i += 1) {
+//     colors.push(
+//       Highcharts.color(base)
+//         .brighten((i - 3) / 7)
+//         .get(),
+//     )
+//   }
+//   return colors
+// }
 
 type Props = {
   data: ChartModel[]
