f9d258a433ebd847a4eb289a837fd3137e7f3f2d
Jorge <jlimajorge@gmail.com>
2021-10-15 14:30:59 -0300

    refator: definindo níveis de acesso para controllers da dashboard

--- api_pdb/app/policies/employee_policy.rb
+++ api_pdb/app/policies/employee_policy.rb
@@ -2,4 +2,12 @@ class EmployeePolicy < ApplicationPolicy
   def admin_or_rh?
     user.has_role?(:admin) || user.has_role?(:rh)
   end
+
+  def admin?
+    user.has_role?(:admin)
+  end
+
+  def admin_or_visitor?
+    user.has_role?(:admin) || user.has_role?(:visitor)
+  end
 end
