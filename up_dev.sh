99b83be0b6af89511685e4b3be794d14eb87c9fc
Jorge <jlimajorge@gmail.com>
2021-10-19 14:57:54 -0300

    chore: adicionando frontend ao docker

--- up_dev.sh
+++ up_dev.sh
@@ -3,6 +3,7 @@
 args="'$*'"
 
 containerRepositoryBackend='api_pdb_dev'
+containerRepositoryFrontend='pdb_frontend'
 
 docker-compose down
 
@@ -10,9 +11,11 @@ docker-compose down
 docker volume rm web_pdb_shared_data web_pdb_gem_cache_dev
 # Delete image
 docker rmi $containerRepositoryBackend:latest
+docker rmi $containerRepositoryFrontend:latest
 
 # Create images
 docker build -t $containerRepositoryBackend:latest --build-arg USER_ID="${USER_ID:-1000}" --build-arg GROUP_ID="${GROUP_ID:-1000}" -f ./api_pdb/Dockerfile ./api_pdb
+docker build -t $containerRepositoryFrontend:latest -f ./frontend/Dockerfile ./frontend
 
 # Execute docker compose to start services described in "docker-compose.yml"
 # Adding "-d" hide logs. To show logs execute "docker-compose logs"
